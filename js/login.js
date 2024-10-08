912111ef3c2b75a2230b25b28a428cd13875510b
yecibeth rivera <riverayecibeth@gmail.com>
2024-10-08 17:05:47 -0500

    login

--- js/login.js
+++ js/login.js
@@ -24,3 +24,68 @@ function activateCurrentNavLink() {
 
 // Llamar a la función al cargar la página
 window.onload = activateCurrentNavLink;
+
+
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('errorMessage');
+
+    errorMessage.textContent = ''; // Limpiar mensaje de error
+
+    // Obtener los usuarios almacenados
+    const users = JSON.parse(localStorage.getItem('formData')) || [];
+
+    // Verificar que los campos no estén vacíos
+    if (!email || !password) {
+        errorMessage.textContent = 'Por favor, completa todos los campos.';
+        return;
+    }
+
+    // Verificar si las credenciales son válidas
+    const user = users.find(user => user.email === email && user.password === password);
+
+  // Verificar si el correo es de un administrador
+  if (email.endsWith('realestatexpress.com')) { // Cambia @empresa.com por el dominio de la empresa
+    Swal.fire({
+        title: '¡Inicio de sesión como administrador!',
+        text: 'Serás redirigido a la vista de administrador.',
+        icon: 'success',
+        confirmButtonText: 'Aceptar'
+    }).then(() => {
+        window.location.href = "admin.html"; // Redirige a la vista de administrador
+    });
+    return; // Salimos de la función si es un administrador
+}
+
+
+if (user) {
+    Swal.fire({
+        title: '¡Inicio de sesión exitoso!',
+        text: 'Serás redirigido a la página de inicio.',
+        icon: 'success',
+        confirmButtonText: 'Aceptar'
+    }).then(() => {
+        window.location.href = "inicio.html"; // Redirige a la vista de usuario normal
+    });
+} else {
+    Swal.fire({
+        title: 'Error',
+        text: 'Email o contraseña inválidos.',
+        icon: 'error',
+        confirmButtonText: 'Intentar de nuevo'
+    });
+    errorMessage.textContent = 'Email o contraseña inválidos.';
+}
+});
+// Evento para alternar la visibilidad de la contraseña
+const togglePassword = document.getElementById('togglePassword');
+togglePassword.addEventListener('click', function () {
+    const passwordField = document.getElementById('password');
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+    this.classList.toggle('fa-eye-slash');
+    this.classList.toggle('fa-eye');
+});
