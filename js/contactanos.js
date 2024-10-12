430dbe9162dbad999265fe1c19182107b3654590
amer009 <amer00966@gmail.com>
2024-10-11 23:14:53 -0500

    Actualizaciones de nav y vista inicio

--- js/contactanos.js
+++ js/contactanos.js
@@ -1,7 +1,7 @@
 // Función para activar el enlace del navbar correspondiente a la vista actual
 function activateCurrentNavLink() {
     // Obtener todos los enlaces del navbar
-    const navbarLinks = document.querySelectorAll('.navbar-links .navtex');
+    const navbarLinks = document.querySelectorAll('.nav-link');
 
     // Obtener el path actual de la URL (sin la parte del dominio)
     const currentPath = window.location.pathname.split('/').pop();
@@ -11,15 +11,14 @@ function activateCurrentNavLink() {
         // Obtener solo el último fragmento del href del enlace
         const linkPath = link.getAttribute('href').split('/').pop();
         
-        // Si coincide con la ruta actual, agregar la clase 'active'
+        // Si coincide con la ruta actual, agregar la clase 'active' y cambiar el color
         if (linkPath === currentPath) {
             link.classList.add('active');
             link.style.color = '#4CAF50'; // Cambia el color a verde
         } else {
+            link.classList.remove('active');
             link.style.color = 'white'; // Restablecer el color para otros enlaces
         }
     });
 }
-
-// Llamar a la función al cargar la página
-window.onload = activateCurrentNavLink;
+window.addEventListener('load', activateCurrentNavLink);
